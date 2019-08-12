77b68a91e563738675df1c4ab1c0615fb403e207
Kathleen <jbobyberry@gmail.com>
2019-08-12 13:39:09 -0400

    xxx

--- app/src/main/java/com/example/parstagram/LogoutActivity.java
+++ app/src/main/java/com/example/parstagram/LogoutActivity.java
@@ -3,27 +3,38 @@ package com.example.parstagram;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.parse.ParseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class LogoutActivity extends AppCompatActivity {
     private ImageView ivHome;
     private ImageView ivNewPost;
     private ImageView ivUserLogout;
-
+    private Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_logout);
         ivHome= findViewById(R.id.ivHome);
         ivNewPost = findViewById(R.id.ivNewPost);
         ivUserLogout = findViewById(R.id.ivUserLogout);
+        btnLogout=findViewById(R.id.btnLogout);
 
+
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser(); // this will now be null
+                goLoginActivity(currentUser);
+            }
+        });
 
         ivHome.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,14 +59,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
-
-
     }
+
     private void goLoginActivity(ParseUser currentUser) {
         Intent i = new Intent(this,LoginActivity.class);
         startActivity(i);
     }
-
 }
